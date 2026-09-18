072115d7fb3623f938621f5e61522aa6472e6f67
mapalohambiliki-hub <mapalohambiliki@gmail.com>
2026-09-18 16:25:04 +0000

    Completed lab1a in class

--- lab1a.py
+++ lab1a.py
@@ -1,15 +1,19 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author:Mapalo Hambiliki
+# Date:18/9/2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: python3 lab1a.py
 
-# TO DO 1: Creating and using varibales
-# create a variable called message.
-# Set the variable to equal to "Welcome to PRG101".
-# Print the variable message using print() statement.
+message="Welcome to PRG101"
+print(message)
+print(type(message))
+newsriting=(message+"Python is future")
+print(newsriting)
+x=10
+print(type(x))
+
 
 # TO DO 2: Checking the type of a varibale
 # Use the builtin type() function and print the type of this variable.
